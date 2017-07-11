c488773f65ac0437f1ca3fa5b72d01a83c040a02
Gaurav Bajaj <gaurav.bajaj@calsoftinc.com>
2017-07-11 06:10:58 -0700

    stp enabled in initial ztp. pn_l2 and pn_l3 scripts checked. Flags are there but stp is suppossed to be enabled using the code

--- ansible/library/pn_initial_ztp.py
+++ ansible/library/pn_initial_ztp.py
@@ -634,12 +634,12 @@ def main():
         enable_web_api(module)
 
     # Disable STP
-    if 'Success' in modify_stp_local(module, 'disable'):
+    if 'Success' in modify_stp_local(module, 'enable'):
         CHANGED_FLAG.append(True)
 
     results.append({
         'switch': current_switch,
-        'output': 'STP disabled'
+        'output': 'STP enabled'
     })
 
     # Enable ports
